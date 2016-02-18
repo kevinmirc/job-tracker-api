1b50aa7d80946f75900315f4b13e76fa54e41ac9
Matteo  Ziff <matteoziff@Matteos-MacBook-Pro.local>
2016-02-18 16:21:25 -0500

    added user model and authentication on api

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.authenticate(user_params)
+    if user
+      data = {
+        token: user.authentication_token,
+        email: user.email
+      }
+      render json: data, status: 201
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+  end
+end
