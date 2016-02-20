fabe8aadbc0d2eee34fe9b025643478ce9d6f6c1
prakashwaghwani <pwaghwani@gmail.com>
2016-02-20 17:14:32 -0500

    added attachment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218222519) do
+ActiveRecord::Schema.define(version: 20160219204557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160218222519) do
     t.integer  "opportunity_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "attachment"
   end
 
   add_index "notes", ["opportunity_id"], name: "index_notes_on_opportunity_id", using: :btree
@@ -63,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160218222519) do
     t.string   "authentication_token"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.string   "attachment"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
